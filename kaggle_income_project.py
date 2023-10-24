560de7948b480bc683a2706e4e9a1a7e0c434e83
MagusConjurer <cameronisadavis@gmail.com>
2023-10-24 11:33:11 -0600

    Adjust how data is loaded to support using the sklearn functions

--- kaggle_income_project.py
+++ kaggle_income_project.py
@@ -8,45 +8,38 @@ Date Created: 24 October 2023
 Modified : 24 October 2023
 """
 
-import sklearn
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.feature_extraction import DictVectorizer
 
 
-def load_data_description(filename):
-    try:
-        attributes = []
-        if filename.endswith('.txt'):
-            with open(filename, 'r') as f:
-                for line in f:
-                    if line.startswith("| attributes"):
-                        next(f)
-                        attr_line = next(f)
-                        attributes = attr_line.replace("\n", "").split(", ")
-                attributes.append("label")
-                attributes.append("weight")
-                return attributes
-        else:
-            print('Provided file', filename, 'is not a txt.')
-    except OSError as e:
-        print('{0} : {1}'.format(e.strerror, e.filename))
-
-
-def load_dataset(filename, attr_list):
+def load_dataset(filename):
     try:
         if filename.endswith('.csv'):
             with open(filename, 'r') as f:
                 data = []
+                labels = []
+                attributes = []
+                vectorizer = DictVectorizer()
                 for line in f:
-                    row = {}
                     entry = line.strip().split(',')
-                    for i in range(len(entry)):
-                        # See if it can be converted to an int before adding to row
-                        try:
-                            value = float(entry[i])
-                        except ValueError:
-                            value = entry[i]
-                        row[attr_list[i]] = value
-                    data.append(row)
-            return data
+                    if len(attributes) < 1:
+                        attributes = entry
+                    else:
+                        row = {}
+                        for i in range(len(entry)):
+                            # See if it can be converted to an int before adding to row
+                            try:
+                                value = float(entry[i])
+                            except ValueError:
+                                value = entry[i]
+
+                            if "income>50K" in attributes[i]:
+                                labels.append(value)
+                            else:
+                                row[attributes[i]] = value
+
+                        data.append(vectorizer.fit_transform(row).toarray())
+            return data, labels
         else:
             print('Provided file', filename, 'is not a csv.')
     except OSError as e:
@@ -56,19 +49,27 @@ def load_dataset(filename, attr_list):
 
 
 def load_data():
-    attr_list = load_data_description("dataset/data-desc.txt")
-    if len(attr_list) > 0:
-        train_data = load_dataset("dataset/train.csv", attr_list)
-        test_data = load_dataset("dataset/test.csv", attr_list)
 
-        return train_data, test_data
+    train_X, train_y = load_dataset("dataset/train.csv")
+    test_X = load_dataset("dataset/test.csv")
+
+    return train_X, train_y, test_X
 
 
 class KaggleProject:
     def __init__(self):
         self.results = []
+        self.training_X, self.training_y, self.testing_X = load_data()
 
-    def __load_data(self):
-        self.__load_dataset("dataset/train.csv")
-        self.__load_dataset("dataset/test.csv")
+    def run_midterm_progress(self):
+        adaBoost = AdaBoostClassifier()
+        adaBoost.fit(self.training_X, self.training_y)
+        print("ID,Prediction")
+        for i in range(1, len(self.testing_X)):
+            row = self.testing_X[i]
+            prediction = adaBoost.predict(row)
+            print("{}, {}".format(i, prediction))
 
+
+project = KaggleProject()
+project.run_midterm_progress()
